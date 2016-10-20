37c9df8281a00d8e9148409e07c210b6e8fcc29a
RyanKong <gongjianjun1@huawei.com>
2016-10-20 21:43:43 +0800

    update to adapt rootfs for armel
    update to adapt rootfs for armel

--- create_rootfs.sh
+++ create_rootfs.sh
@@ -24,7 +24,7 @@ elif [ "$1" == "arm64" ]; then
     OPTION="--foreign --arch arm64"
 else
     echo 'usage:'
-    echo '       ./create_rootfs.sh {powerpc|armel|arm64|x86_64}'
+    echo ' ./create_rootfs.sh {powerpc|armel|arm64|x86_64}'
     echo ' '
     exit 1
 fi
@@ -68,15 +68,71 @@ echo "EOF" >>changepasswd.sh
 chmod u+x changepasswd.sh
 #创建inittab
 echo "::sysinit:/etc/init.d/rcS">inittab
-echo "::askfirst:-/bin/login">>inittab
+#echo "::askfirst:-/bin/login">>inittab
 echo "tty1::askfirst:-/bin/login">>inittab
-echo "tty2::askfirst:-/bin/login">>inittab
+#echo "tty2::askfirst:-/bin/login">>inittab
 echo "::restart:/sbin/init">>inittab
 echo "::ctrlaltdel:/sbin/reboot">>inittab
 echo "::shutdown:/bin/umount -a -r">>inittab
 echo "::shutdown:/sbin/swapoff -a">>inittab
  
 CHROOT="sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot "
+
+if [ "$1" == "armel"];then
+OCAL_CHROOT="${CHROOT} ${TARGET}"
+    sudo debootstrap ${OPTION} ${SUITE} ${TARGET} ${MIRROR} \
+    && sudo cp /usr/bin/${QEMU_STATIC_BIN} ${TARGET}/usr/bin/ \
+    && sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot ${TARGET}/ /debootstrap/debootstrap --second-stage \
+    && ${LOCAL_CHROOT} sh -c 'echo "deb http://ftp.cn.debian.org/debian/ jessie main non-free contrib">/etc/apt/sources.list' \
+    && ${LOCAL_CHROOT} sh -c 'echo "deb http://ftp.cn.debian.org/debian/ jessie-updates main non-free contrib">>/etc/apt/sources.list' \
+    && ${LOCAL_CHROOT} sh -c 'echo "deb-src http://ftp.cn.debian.org/debian/ jessie main non-free contrib">>/etc/apt/sources.list' \
+    && ${LOCAL_CHROOT} sh -c 'echo "deb-src http://ftp.cn.debian.org/debian/ jessie-updates main non-free contrib">>/etc/apt/sources.list' \
+    && ${LOCAL_CHROOT} apt-get update \
+    && ${LOCAL_CHROOT} apt-get --force-yes -y purge libboost-iostreams1.55.0 cpio cron gcc-4.8-base libgdbm3 \
+       groff-base libicu52 init-system-helpers libxtables10 iptables libjson-c2 \
+       libestr0 libidn11 liblogging-stdlog0 liblognorm1 libmnl0 libnetfilter-acct1 \
+       libnfnetlink0 libpipeline1 libpsl0 libsigc++-2.0-0c2a libtext-charwidth-perl \
+       libtext-iconv-perl libtext-wrapi18n-perl logrotate man-db manpages nano \
+       netcat-traditional libnewt0.52 nfacct libssl1.0.0 libpopt0 tasksel-data tasksel wget \
+       debconf-i18n \
+    && ${LOCAL_CHROOT} apt-get install --force-yes -y --no-install-recommends openssh-server \
+       ftp telnet ssh libxslt1.1 libxml2 libidn11 librtmp1 libssh2-1 libldap-2.4-2 libcurl4-openssl-dev vim \
+       tree openssl ntp  binutils gcc sudo psmisc gdb \
+    && ${LOCAL_CHROOT} apt-get upgrade \
+    && ${LOCAL_CHROOT} apt-get download busybox \
+    && ${LOCAL_CHROOT} sh -c 'dpkg -X busybox*.deb /busybox' \
+    && ${LOCAL_CHROOT} sh -c 'rm -f busybox*.deb' \
+    && ${LOCAL_CHROOT} sh -c 'cp /busybox/bin/busybox /bin/busybox' \
+    && ${LOCAL_CHROOT} sh -c 'rm -f /bin/login' \
+    && ${LOCAL_CHROOT} sh -c 'ln -s /bin/busybox /bin/login' \
+    && ${LOCAL_CHROOT} sh -c 'rm -f /sbin/sulogin' \
+    && ${LOCAL_CHROOT} sh -c 'ln -s /bin/busybox /sbin/sulogin' \
+    && ${LOCAL_CHROOT} sh -c 'rm -f /linuxrc' \
+    && ${LOCAL_CHROOT} sh -c 'ln -s /bin/busybox /linuxrc' \
+    && ${LOCAL_CHROOT} sh -c 'rm /sbin/init' \
+    && ${LOCAL_CHROOT} sh -c 'ln -s /bin/busybox /sbin/init' \
+    && ${LOCAL_CHROOT} sh -c 'rm -rf /busybox' \
+    && sudo cp inittab ${TARGET}/etc/ \
+    && sudo cp clean.sh ${TARGET}/ \
+    && sudo cp changepasswd.sh ${TARGET}/ \
+    && ${LOCAL_CHROOT} /clean.sh \
+    && ${LOCAL_CHROOT} /changepasswd.sh \
+    && sudo rm ${TARGET}/clean.sh \
+    && sudo rm ${TARGET}/changepasswd.sh \
+    && ${LOCAL_CHROOT} sh -c 'echo "pts/x" >> /etc/securetty' \
+    && ${LOCAL_CHROOT} sh -c 'echo "CE" > /etc/hostname' \
+    && ${LOCAL_CHROOT} sh -c 'echo "127.0.0.1 CE" >> /etc/hosts' \
+    && ${LOCAL_CHROOT} sh -c 'hostname CE' \
+    && ${LOCAL_CHROOT} sh -c 'sed -ie "s/^trap - EXIT # Disable emergency handler$/\/etc\/init.d\/ssh restart \ntrap - EXIT # Disable emergency handler/" /etc/init.d/rc' \
+    && ${LOCAL_CHROOT} sh -c 'sed -ie "s/^PermitRootLogin without-password$/PermitRootLogin yes/" /etc/ssh/sshd_config' \
+    && sudo rm ${TARGET}/usr/bin/${QEMU_STATIC_BIN} \
+    && rm clean.sh changepasswd.sh inittab \
+    && ROOTFS_NAME="${TARGET}-`date +'%Y-%m-%d-%H-%M'`".tar.gz \
+    && echo ${ROOTFS_NAME} \
+    && sudo tar -czf ${ROOTFS_NAME} ${TARGET}/ \
+    && echo "create rootfs success!" \
+    && exit 0
+else
 LOCAL_CHROOT="${CHROOT} ${TARGET}"
     sudo debootstrap ${OPTION} ${SUITE} ${TARGET} ${MIRROR} \
     && sudo cp /usr/bin/${QEMU_STATIC_BIN} ${TARGET}/usr/bin/ \
@@ -130,7 +186,7 @@ LOCAL_CHROOT="${CHROOT} ${TARGET}"
     && sudo tar -czf ${ROOTFS_NAME} ${TARGET}/ \
     && echo "create rootfs success!" \
     && exit 0
- 
+fi 
  
     echo "create rootfs failed!"
     exit 1
